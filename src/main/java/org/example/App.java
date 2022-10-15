2bd1df7727caf7d3f691347352ed04866b6ee8d1
ErickBRAlessi <erick_alessi@outlook.com>
2022-10-15 00:53:24 -0300

    pseudo physics

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,32 +2,32 @@ package org.example;
 
 import javafx.animation.AnimationTimer;
 import javafx.animation.Timeline;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
-    private static final int SCENE_SIZE_X = 1920;
-    private static final int SCENE_SIZE_Y = 1080;
+    //physics calculated with 10px = 1m
+    private static final int SCENE_SIZE_X = 1000;
+    private static final int SCENE_SIZE_Y = 1000;
     //main timeline
     private Timeline timeline;
-    private AnimationTimer timer;
+    private AnimationTimer eventTimer;
     private Integer frame = 0;
+    private Double fps = 0d;
+    private Double secondFromTheLastFrame;
 
     @Override
     public void start(Stage stage) {
@@ -36,76 +36,77 @@ public class App extends Application {
         stage.setTitle("What Am I doing with my life?");
         stage.setScene(scene);
         stage.show();
-
-        final Text text = new Text(frame.toString());
-        text.setStroke(Color.WHITE);
-        //You can add a specific action when each frame is started.
-        timer = new AnimationTimer() {
+        addFrameCounter(root);
+        eventTimer = new AnimationTimer() {
             @Override
             public void handle(long l) {
-                text.setText(frame.toString());
-                frame++;
+                root.getChildren().stream()
+                        .filter(c -> c instanceof CirclePhysics)
+                        .map(c -> (CirclePhysics) c)
+                        .forEach(c -> {
+                            c.refreshPosition(secondFromTheLastFrame);
+                        });
             }
         };
-
+        eventTimer.start();
         EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-                var circle = new Circle(e.getSceneX(), e.getSceneY(), 10, Color.WHITE);
-                circle.setFill(Color.WHITE);
+                CirclePhysics circle = new CirclePhysics(e.getSceneX(), e.getSceneY(), 10, Color.WHITE);
                 root.getChildren().add(circle);
-                //Creating the translation transformation
-                fallAnimation(e.getSceneY(), circle);
-            }
-
-            private void fallAnimation(double high, Circle circle) {
-                TranslateTransition translateTransition = new TranslateTransition();
-                //Setting the X,Y,Z coordinates to apply the translation
-                translateTransition.setDuration(Duration.millis(getFallMilliSecondsCalculation(high)));
-                //Setting the node for the transition
-                translateTransition.setNode(circle);
-                //Setting the value of the transition along the y axis.
-                translateTransition.setByY(SCENE_SIZE_Y - high);
-                //Setting the cycle count for the transition
-                translateTransition.setCycleCount(1);
-                //Setting auto reverse value to false
-                translateTransition.setAutoReverse(false);
-                //Playing the animation
-                translateTransition.play();
             }
         };
 
         EventHandler<KeyEvent> pumpEventClick = new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent e) {
-                root.getChildren().stream().forEach(node -> {
-                    double random = Math.random() * SCENE_SIZE_Y;
-                    //Creating the translation transformation
-                    TranslateTransition translateTransition = new TranslateTransition();
-                    //Setting the X,Y,Z coordinates to apply the translation
-                    translateTransition.setDuration(Duration.millis(getFallMilliSecondsCalculation(random)));
-                    //Setting the node for the transition
-                    translateTransition.setNode(node);
-                    //Setting the value of the transition along the y axis.
-                    translateTransition.setByY(-random);
-                    //Setting the cycle count for the transition
-                    translateTransition.setCycleCount(2);
-                    //Setting auto reverse value to false
-                    translateTransition.setAutoReverse(true);
-                    //Playing the animation
-                    translateTransition.play();
-                    EventHandler onFinished = new EventHandler<ActionEvent>() {
-                        public void handle(ActionEvent t) {
-                            node.setTranslateX(java.lang.Math.random() * 200 - 100);
-                        }
-                    };
-                });
 
             }
         };
-        scene.addEventHandler(MouseEvent.MOUSE_DRAGGED, eventHandler);
+        scene.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler);
         scene.addEventHandler(KeyEvent.KEY_TYPED, pumpEventClick);
 
+    }
+
+    private void addFrameCounter(Group root) {
+        timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        Text frameText = new Text(10, 20, frame.toString());
+        frameText.setStroke(Color.WHITE);
+        Text fpsText = new Text(10, 40, "0");
+        fpsText.setStroke(Color.WHITE);
+        //You can add a specific action when each frame is started.
+        AnimationTimer frameTimer = new AnimationTimer() {
+            long delta;
+            long lastFrameTime;
+
+            @Override
+            public void handle(long now) {
+                delta = now - lastFrameTime;
+                lastFrameTime = now;
+                refreshFrameStatus();
+                setFrameStatusTexts();
+            }
+
+            public double getFrameRateHertz() {
+                double frameRate = 1d / delta;
+                return frameRate * 1e9;
+            }
+
+            private void refreshFrameStatus() {
+                frame++;
+                fps = getFrameRateHertz();
+                secondFromTheLastFrame = 1d / fps;
+            }
+
+            private void setFrameStatusTexts() {
+                frameText.setText(frame.toString());
+                fpsText.setText(Double.toString(fps));
+            }
+        };
+        frameTimer.start();
+        root.getChildren().add(frameText);
+        root.getChildren().add(fpsText);
     }
 
 
@@ -113,25 +114,60 @@ public class App extends Application {
         launch();
     }
 
+    private class CirclePhysics extends Circle {
+        private double accelerationX = 0d; // m/s²
+        private double velocityX = 0d; // m/s
+        private double accelerationY = 0d; // m/s²
+        private double velocityY = 0d; // m/s
+        private double mass = 1d; //kg
 
-    private int getFallMilliSecondsCalculation(double high) {
-        return (int) Math.sqrt(2 * high * 100);
+        CirclePhysics(double x, double y, double size, Paint color) {
+            super(x, y, size, color);
+        }
+
+        void applyForceY(double force) {
+            accelerationY = force / mass;
+        }
+
+        void applyForceX(double force) {
+            accelerationX = force / mass;
+        }
+
+        void refreshPosition(double timePassed) {
+            super.setTranslateX(getTranslateX() + calculateTranslateX(timePassed));
+            super.setTranslateY(getTranslateY() + calculateTranslateY(timePassed));
+        }
+
+        private double calculateTranslateX(double timePassed) {
+            return (velocityX * timePassed) + ((accelerationX * timePassed * timePassed) / 2);
+        }
+
+        private double calculateTranslateY(double timePassed) {
+            return (velocityY * timePassed) + ((accelerationY * timePassed * timePassed) / 2);
+        }
+
+        public void setAccelerationX(double accelerationX) {
+            this.accelerationX = accelerationX;
+        }
+
+        public void setVelocityX(double velocityX) {
+            this.velocityX = velocityX;
+        }
+
+        public void setAccelerationY(double accelerationY) {
+            this.accelerationY = accelerationY;
+        }
+
+        public void setVelocityY(double velocityY) {
+            this.velocityY = velocityY;
+        }
+
+        public void setMass(double mass) {
+            if (mass == 0d) {
+                mass = 1d;
+            }
+            this.mass = mass;
+        }
     }
 
-    /*
-      Timeline timeline = new Timeline();
-        for (Node circle: root.getChildren()) {
-            timeline.getKeyFrames().addAll(
-                    new KeyFrame(Duration.ZERO, // set start position at 0
-                            new KeyValue(circle.translateXProperty(), Math.random() * 1920),
-                            new KeyValue(circle.translateYProperty(), Math.random() * 1080)
-                    ),
-                    new KeyFrame(new Duration(40000), // set end position at 40s
-                            new KeyValue(circle.translateXProperty(), Math.random() * 1920),
-                            new KeyValue(circle.translateYProperty(), Math.random() * 1080)
-                    )
-            );
-        }
-timeline.play();
-     */
 }
